3e8d5c4eb4fc94ce0b240fe0d74cb6a7d5b7e850
rebecca_alvarez <realvarez.dwfc5@ucamp.utel.edu.mx>
2022-08-26 19:54:41 -0500

    href

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -26,7 +26,7 @@ class Footer extends Component {
                 <li>&copy; Copyright 2022 ReAl Projects</li>
                 <li>
                   Design by{" "}
-                  <a title="Styleshout" href="">
+                  <a title="Styleshout">
                     Rebecca AlMe
                   </a>
                 </li>
